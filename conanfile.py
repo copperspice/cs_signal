42f437be01737136299ca1df7e6aaa755cbace98
Paul M. Bendixen <paulbendixen@gmail.com>
2022-03-28 22:41:35 +0200

    Conan: Update to use cmake install

--- conanfile.py
+++ conanfile.py
@@ -22,32 +22,18 @@ class CsSignalConan(ConanFile):
     def validate(self):
         tools.check_min_cppstd(self,"17")
 
-    def source(self):
-        # This small hack might be useful to guarantee proper /MT /MD linkage
-        # in MSVC if the packaged project doesn't have variables to set it
-        # properly
-        tools.replace_in_file("CMakeLists.txt", "project(cs_signal)",
-                              '''project(cs_signal)
-include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
-conan_basic_setup()''')
-
-    def build(self):
+    def _configure(self):
         cmake = CMake(self)
         cmake.configure()
+        return cmake
+
+    def build(self):
+        cmake = self._configure()
         cmake.build()
 
-        # Explicit way:
-        # self.run('cmake %s/hello %s'
-        #          % (self.source_folder, cmake.command_line))
-        # self.run("cmake --build . %s" % cmake.build_config)
-
     def package(self):
-        self.copy("*.h", dst="include", src="src")
-        self.copy("*CsSignal.lib", dst="lib", keep_path=False)
-        self.copy("*.dll", dst="bin", keep_path=False)
-        self.copy("*.so", dst="lib", keep_path=False)
-        self.copy("*.dylib", dst="lib", keep_path=False)
-        self.copy("*.a", dst="lib", keep_path=False)
+        cmake = self._configure()
+        cmake.install()
 
     def package_info(self):
         self.cpp_info.libs = ["CsSignal"]
