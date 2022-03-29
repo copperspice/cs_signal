b5915b98171f242aad9640ade1b61b9c269da8d6
Barbara Geller <barbara@copperspice.com>
2022-03-29 11:09:27 -0700

    Add CMake export files

--- src/signal.cmake
+++ src/signal.cmake
@@ -12,16 +12,26 @@ target_compile_features(
    cxx_std_17
 )
 
+target_include_directories(
+   CsSignal
+   PUBLIC
+   $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/src>
+)
+
 target_sources(CsSignal
    PRIVATE
    ${CMAKE_CURRENT_SOURCE_DIR}/src/cs_signal.cpp
    ${CMAKE_CURRENT_SOURCE_DIR}/src/cs_slot.cpp
 )
 
-target_include_directories(
-   CsSignal
-   PUBLIC
-   $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/src>
+set(CS_SIGNAL_INCLUDES
+   ${CMAKE_CURRENT_SOURCE_DIR}/src/cs_internal.h
+   ${CMAKE_CURRENT_SOURCE_DIR}/src/cs_macro.h
+   ${CMAKE_CURRENT_SOURCE_DIR}/src/cs_signal.h
+   ${CMAKE_CURRENT_SOURCE_DIR}/src/cs_slot.h
+
+   ${CMAKE_CURRENT_SOURCE_DIR}/src/cs_rcu_guarded.h
+   ${CMAKE_CURRENT_SOURCE_DIR}/src/cs_rcu_list.h
 )
 
 if(MSVC)
@@ -37,26 +47,22 @@ if(MSVC)
    endif()
 endif()
 
-set(CS_SIGNAL_INCLUDE
-   ${CMAKE_CURRENT_SOURCE_DIR}/src/cs_internal.h
-   ${CMAKE_CURRENT_SOURCE_DIR}/src/cs_macro.h
-   ${CMAKE_CURRENT_SOURCE_DIR}/src/cs_signal.h
-   ${CMAKE_CURRENT_SOURCE_DIR}/src/cs_slot.h
-
-   ${CMAKE_CURRENT_SOURCE_DIR}/src/cs_rcu_guarded.h
-   ${CMAKE_CURRENT_SOURCE_DIR}/src/cs_rcu_list.h
-)
-
 install(
    TARGETS CsSignal
+   EXPORT CsSignalLibraryTargets ${INSTALL_TARGETS_DEFAULT_ARGS}
    RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR}
    LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
    ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR}
 )
 
 install(
-   FILES ${CS_SIGNAL_INCLUDE}
+   FILES ${CS_SIGNAL_INCLUDES}
    DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}
    COMPONENT Devel
 )
 
+install(
+   EXPORT CsSignalLibraryTargets
+   FILE CsSignalLibraryTargets.cmake
+   DESTINATION ${PKG_PREFIX}
+)
